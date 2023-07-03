79ff3e2617c75dc9ad0979936083f232910cdbce
Shodiyor Shukurov <shodiyorshukurov07@gmail.com>
2023-07-03 09:31:16 +0500

    Country end

--- src/components/Pagination/index.jsx
+++ src/components/Pagination/index.jsx
@@ -1,6 +1,12 @@
+import React from "react";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { Context as ThemeContext } from "../../context/ThemeContext";
+import "./pagination.scss";
 
 const Paginition = ({ countriesPerPage, totalCountries, paginate }) => {
+
+  const { theme } = React.useContext(ThemeContext);
   const pageNumbers = [];
 
   for (let i = 1; i <= Math.ceil(totalCountries / countriesPerPage); i++) {
@@ -9,21 +15,28 @@ const Paginition = ({ countriesPerPage, totalCountries, paginate }) => {
 
   return (
     <>
-      <ul className="pagination">
-        {pageNumbers.map((number) => (
-          <li className="page-item" key={number}>
-            <Link
-              to={"/home"}
-              className="page-link"
-              onClick={() => paginate(number)}
-            >
-              {number}
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <section className="pagination-section">
+        <ul className=" pagination-section__list">
+          {pageNumbers.map((number) => (
+            <li className={`page-item pagination-section__item  header--${theme}`} key={number}>
+              <Link
+                to={"/home"}
+                className="page-link pagination-section__link"
+                onClick={() => paginate(number)}
+              >
+                {number}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </section>
     </>
   );
 };
 
+Paginition.propTypes = {
+  countriesPerPage: PropTypes.number,
+  totalCountries: PropTypes.number,
+  paginate: PropTypes.func,
+};
 export default Paginition;
